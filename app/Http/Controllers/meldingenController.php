8d5f21c765932105514647e34770aefca8f5ac6e
Arnoud2208 <arnoudvoeten09@gmail.com>
2026-02-12 11:37:59 +0100

    Update meldingenController.php

--- app/Http/Controllers/meldingenController.php
+++ app/Http/Controllers/meldingenController.php
@@ -1,17 +1,29 @@
 <?php
 
-//Variabelen vullen
-$attractie = $_POST['attractie'];
+// Variabelen vullen
+$attractie  = $_POST['attractie'];
 $capaciteit = $_POST['capaciteit'];
-$melder = $_POST['melder'];
+$melder     = $_POST['melder'];
 
 echo $attractie . " / " . $capaciteit . " / " . $melder;
 
-//1. Verbinding
+// 1. Verbinding
 require_once '../../../config/conn.php';
 
-//2. Query
+// 2. Query
+$sql = "INSERT INTO meldingen (attractie, capaciteit, melder)
+        VALUES (:attractie, :capaciteit, :melder)";
 
-//3. Prepare
+// 3. Prepare
+$stmt = $conn->prepare($sql);
 
-//4. Execute
+// 4. Execute
+$stmt->execute([
+    ':attractie'  => $attractie,
+    ':capaciteit' => $capaciteit,
+    ':melder'     => $melder
+]);
+
+echo "Gegevens succesvol opgeslagen!";
+?>
+
